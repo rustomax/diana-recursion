374f9fc07d3f691a075e8534eaea5f685074a3aa
Max Skybin <git@skybin.net>
2016-12-01 23:31:01 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+// Simple recursion example for Diana
+
+// print_digits function calls itself
+fn print_digits(square_size: i32, digit_type: i32) {
+    if square_size != 0 {
+        print!("{} ", digit_type);
+        print_digits(square_size - 1, digit_type);
+    }
+}
+
+fn print_line(left_zeros: i32, middle_ones: i32, right_zeros: i32) {
+    print_digits(left_zeros, 0);
+    print_digits(middle_ones, 1);
+    print_digits(right_zeros, 0);
+    println!("");
+}
+
+fn main() {
+    let n = 3;
+    let h = 2;
+    let m = 4;
+
+    for _ in 0..n {
+        print_line (0, n, h + m);
+    }
+
+    for _ in 0..h {
+        print_line (n, h, m);
+    }
+
+    for _ in 0..m {
+        print_line (n + h, m, 0);
+    }
+
+}
+
+// Output:
+// 1 1 1 0 0 0 0 0 0
+// 1 1 1 0 0 0 0 0 0
+// 1 1 1 0 0 0 0 0 0
+// 0 0 0 1 1 0 0 0 0
+// 0 0 0 1 1 0 0 0 0
+// 0 0 0 0 0 1 1 1 1
+// 0 0 0 0 0 1 1 1 1
+// 0 0 0 0 0 1 1 1 1
+// 0 0 0 0 0 1 1 1 1
