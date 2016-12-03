6641ff105dd99be532ab6a0a8a2e92e4d05caee6
Max Skybin <git@skybin.net>
2016-12-03 10:09:19 -0500

    all recursive functions

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,19 @@
 
 // print_digits() function calls itself
 fn print_digits(square_size: i32, digit_type: i32) {
-    if square_size != 0 {
+    if square_size > 0 {
         print!("{} ", digit_type);
         print_digits(square_size - 1, digit_type);
     }
 }
 
-fn print_line(left_zeros: i32, middle_ones: i32, right_zeros: i32) {
-    print_digits(left_zeros, 0);
-    print_digits(middle_ones, 1);
-    print_digits(right_zeros, 0);
-    println!("");
-}
-
 // print_square() function is recursive too
 fn print_square(square_size: i32, left_zeros: i32, middle_ones: i32, right_zeros: i32) {
     if square_size != 0 {
-        print_line(left_zeros, middle_ones, right_zeros);
+        print_digits(left_zeros, 0);
+        print_digits(middle_ones, 1);
+        print_digits(right_zeros, 0);
+        println!("");
         print_square(square_size - 1, left_zeros, middle_ones, right_zeros);
     }
 }
